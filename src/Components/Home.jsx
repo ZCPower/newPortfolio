7b045ae69f092286ca262d311da605e021b022d3
ZCPower <zachpowerxc@gmail.com>
2022-12-07 12:56:47 -0600

    debugging video

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ import '../Styles/Home.css'
 function Home() {
     return (
         <div id='homeContainer' className='flex flex-col justify-center'>
-            <video type='video/mp4' autoPlay='' muted loop src='/src/assets/video.mp4' />
+            <video type='video/mp4' autoPlay muted loop src='../src/assets/video.mp4' />
 
             {/* <video autoPlay muted loop src='https://player.vimeo.com/external/370467553.sd.mp4?s=96de8b923370fb7fa8616d4e0b74eaf3fac9e576&profile_id=164&oauth2_token_id=57447761' /> */}
             <div id='intro' className='gap'>
