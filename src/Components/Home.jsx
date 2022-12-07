49af851094218a9b26ac56a9da0e9528d2c93586
ZCPower <zachpowerxc@gmail.com>
2022-12-07 12:37:22 -0600

    testing video fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,9 +4,9 @@ import '../Styles/Home.css'
 function Home() {
     return (
         <div id='homeContainer' className='flex flex-col justify-center'>
-            {/* <video autoPlay muted loop src='/src/assets/video.mp4'></video> */}
+            <video type='video/mp4' autoPlay='' muted loop src='/src/assets/video.mp4' />
 
-            <video autoPlay muted loop src='https://player.vimeo.com/external/370467553.sd.mp4?s=96de8b923370fb7fa8616d4e0b74eaf3fac9e576&profile_id=164&oauth2_token_id=57447761'></video>
+            {/* <video autoPlay muted loop src='https://player.vimeo.com/external/370467553.sd.mp4?s=96de8b923370fb7fa8616d4e0b74eaf3fac9e576&profile_id=164&oauth2_token_id=57447761' /> */}
             <div id='intro' className='gap'>
                 <h2 className='text-7xl mb-5'>Fullstack Developer</h2>
                 {/* Perhaps add an effect that 'types' a couple things out randomly that describe myself. */}
