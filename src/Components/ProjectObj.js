d78e276f1c10dc4e0e316cc6dd05495eba86267d
ZCPower <zachpowerxc@gmail.com>
2022-12-07 15:21:11 -0600

    adding functionality to projects

--- src/Components/ProjectObj.js
+++ src/Components/ProjectObj.js
@@ -1,4 +1,5 @@
 export const projectArr = [
+    //WEEABOO
     {
         name: 'Weeaboo',
         description: 'The capstone project of my coding bootcamp. An e-commerce website where users can browse through a selection of anime merch and purchase them using Stripe.',
@@ -10,10 +11,34 @@ export const projectArr = [
         completed: 'insert date here...',
         github: 'https://github.com/ZCPower/GraceShopper'
     },
+
+
+    // FitnessTRACKER
     { name: 'FitnessTrackr', description: "Create an account and build exercise routines. I'm fergalicious, my body stay vicious. I be up in the gym just working on my fitness. He's my witness. Ooooo-weeee!", logo: "https://img.icons8.com/officel/2x/shutdown.png", link: 'https://fitness-zp.netlify.app/', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', },
-    { name: "Stranger's Things", logo: 'https://img.icons8.com/cotton/2x/box.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'A Craiglist-Lite App built using React' },
-    { name: 'Tic Tac Toe', logo: 'https://cdn-icons-png.flaticon.com/128/1021/1021366.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'Built with JavaScript to learn how to manipulate the DOM. This game is out of this world!' },
-    { name: 'Art Collector', logo: 'https://cdn-icons-png.flaticon.com/128/1048/1048944.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'An App built to practice API calls and React. Browse the Harvard Art Museum from the comfort of your home!' },
+
+    //STRANGERSTHINGS
+    {
+        name: "Stranger's Things", logo: 'https://img.icons8.com/cotton/2x/box.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'A Craiglist-Lite App built using React',
+        link: 'https://strangersthings-zp.netlify.app/',
+        github: 'https://github.com/ZCPower/StrangersThings2'
+    },
+
+
+    //TIC TAC TOE
+    { name: 'Tic Tac Toe', logo: 'https://cdn-icons-png.flaticon.com/128/1021/1021366.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'Built with JavaScript to learn how to manipulate the DOM. This game is out of this world!', link: 'https://funny-donut-346f04.netlify.app/', github: 'https://github.com/ZCPower/Arcade' },
+
+    //ART COLLECTOR
+    {
+        name: 'Art Collector', logo: 'https://cdn-icons-png.flaticon.com/128/1048/1048944.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'An App built to practice API calls and React. Browse the Harvard Art Museum from the comfort of your home!',
+        link: 'https://lambent-chebakia-68ed4c.netlify.app/',
+        github: 'https://github.com/ZCPower/UNIV_Art_Collector'
+    },
+
+
+    //SCHEDULER
     { name: 'Scheduler', logo: 'https://cdn-icons-png.flaticon.com/128/3813/3813747.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'An application that will allow an ABA Clinic to easily schedule their patients and technicians!' },
-    { name: 'Quizler', logo: 'https://cdn-icons-png.flaticon.com/128/3406/3406898.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'An application built to help me prepare so interviews. It is intended to be a Quizlet/Chegg clone.' }
+
+
+    //QUIZLER
+    { name: 'Quizler', logo: 'https://cdn-icons-png.flaticon.com/128/3406/3406898.png', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Zrgzyq0nG2A11DlZHlWHlk8L4r-FGGhur1UPcCPe&s', description: 'An application built to help me prepare for interviews. It is intended to be a Quizlet/Chegg clone.' }
 ]
--- src/Components/SingleProject.jsx
+++ src/Components/SingleProject.jsx
@@ -10,12 +10,16 @@ function SingleProject({ name, description, logo, image, link, github }) {
             </div>
             <img src={image}></img>
             <p className='text-base h-32 '>{description}</p>
-            <div className='flex gap-2 justify-center pb-2'>
-                <a href={github} target='_blank'><button className='text-base flex gap-2 items-center'><img className='w-8' src='https://cdn-icons-png.flaticon.com/128/919/919847.png'></img>GitHub</button></a>
-                <a href={link} target='_blank'><button className='text-base flex gap-2 items-center'><img className='w-8' src='https://cdn-icons-png.flaticon.com/128/870/870169.png'></img>Visit Page</button></a>
+            <div id='projButtons' className='flex gap-2 justify-center items-center pb-2'>
+                {github && link ?
+                    <>
+                        <a href={github} target='_blank'><button className='text-base flex gap-2 items-center'><img className='w-8' src='https://cdn-icons-png.flaticon.com/128/919/919847.png'></img>GitHub</button></a>
+
+                        <a href={link} target='_blank'><button className='text-base flex gap-2 items-center'><img className='w-8' src='https://cdn-icons-png.flaticon.com/128/870/870169.png'></img>Visit Page</button></a></> : <h4 className='text-xl text-purple'>Currently under construction!</h4>
+                }
                 {/* PERHAPS BUILD SOMETHING THAT SO WHEN YOU CLICK ON A PROJECT ANYWHERE THATS NOT A BUTTON, YOU CAN VIEW ALL THE DATA OF THE PROJECT i.e. What it was built with... */}
             </div>
-        </div>
+        </div >
     )
 }
 
