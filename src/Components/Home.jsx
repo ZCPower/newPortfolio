12090ebcdb4dbec3e1dd1ecc27004f6535743799
ZCPower <zachpowerxc@gmail.com>
2022-12-07 14:11:33 -0600

    test

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../Styles/Home.css'
+import { videoUrl } from './videoURL'
 
 function Home() {
     return (
         <div id='homeContainer' className='flex flex-col justify-center'>
-            <video type='video/mp4' autoPlay muted loop src='../src/assets/video.mp4' />
+            {/* <video type='video/mp4' autoPlay muted loop src='../src/assets/video.mp4' /> */}
 
-            {/* <video autoPlay muted loop src='https://player.vimeo.com/external/370467553.sd.mp4?s=96de8b923370fb7fa8616d4e0b74eaf3fac9e576&profile_id=164&oauth2_token_id=57447761' /> */}
+            <video autoPlay muted loop src={videoUrl} />
             <div id='intro' className='gap'>
                 <h2 className='text-7xl mb-5'>Fullstack Developer</h2>
                 {/* Perhaps add an effect that 'types' a couple things out randomly that describe myself. */}
