6359f223a52d32d2fc7b62ff7f661214fff8de4e
ZCPower <zachpowerxc@gmail.com>
2022-12-08 15:28:11 -0600

    removed pictureInpictureMode for opera

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <div id='homeContainer' className='flex flex-col justify-center'>
             {/* <video type='video/mp4' autoPlay muted loop src='../src/assets/video.mp4' /> */}
 
-            <video autoPlay muted loop src={videoUrl} />
+            <video autoPlay muted loop disablePictureInPicture controls={false} src={videoUrl} />
             <div id='intro' className='gap px-5'>
                 <h2 className='text-5xl mb-5 pt-2 sm:text-6xl md:text-7xl lg:text-8xl'>Fullstack Developer</h2>
                 {/* Perhaps add an effect that 'types' a couple things out randomly that describe myself. */}
