08a9311b8eaae1142a7340442874c41ade0299ca
ZCPower <zachpowerxc@gmail.com>
2022-12-09 00:17:19 -0600

    tried to build my own typewriter functionality, but the timing was off. used a npm package to take care of it for now

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../Styles/Home.css'
-import { videoUrl } from './videoURL'
+import { videoUrl } from './videoURL';
+
 
 import { motion } from 'framer-motion'
-
+import Typewriter from 'typewriter-effect'
 function Home() {
+    let textArr = [
+        "Hi, I'm Zach.",
+        "I'm a Fullstack Developer. ",
+        "I love to build web apps.",
+        "I can build you one?"
+    ];
+
+    const [greeting, setGreeting] = useState(textArr[0]);
+    const [index, setIndex] = useState(0);
+
+    // useEffect(() => {
+    //     const timerId = setInterval(
+    //         () => setIndex((i) => (i + 1) % textArr.length), // <-- increment index
+    //         5000
+    //     );
+    //     return () => clearInterval(timerId);
+    // }, []);
+
+
+    // useEffect(() => {
+    //     setGreeting(textArr[index]); // <-- update media state when index updates
+    // }, [index]);
+
+
     return (
         <motion.div id='homeContainer' className='flex flex-col justify-center
         '
-        // initial={{ y: 20 }}
-        // animate={{ y: 0 }}
-        // exit={{ y: 20 }}
-        // transition={{ duration: 0.5 }}
         >
-            {/* <video type='video/mp4' autoPlay muted loop src='../src/assets/video.mp4' /> */}
 
             <video autoPlay='autoplay' muted loop disablePictureInPicture controls={false} src={videoUrl} type='video/mp4' />
             <div id='intro' className='gap px-5'>
-                <h2 className='text-5xl mb-5 pt-2 sm:text-6xl md:text-7xl lg:text-8xl'>Fullstack Developer</h2>
+                <h2 className='text-3xl mb-5 my-4 py-2 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl type' ><Typewriter
+                    options={{
+                        strings: ["Hi, I'm Zach.", "I'm a Fullstack developer.", "I love to build web apps."],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                /></h2>
                 {/* Perhaps add an effect that 'types' a couple things out randomly that describe myself. */}
                 {/* <p className='text-3xl'>Fullstack Web Developer</p> */}
             </div>
