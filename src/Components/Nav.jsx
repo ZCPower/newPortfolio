bfb31a091b87c25c31dbf053d0ed801e195d41f6
ZCPower <zachpowerxc@gmail.com>
2022-12-11 13:20:51 -0600

    resized navBar for mobile screens

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,17 +5,17 @@ import { animate, motion } from 'framer-motion'
 
 function Nav() {
     return (
-        <motion.div id='navBar' className='w-full bg-black flex items-center gap-2 justify-around items-center'
+        <motion.div id='navBar' className='w-full bg-black flex items-center sm:gap-2 justify-around items-center'
             initial={{ y: '-10vh' }}
             animate={{ y: 0, transition: { duration: 1 } }}
 
         >
-            <div id='logo' className='flex items-center gap-3'>
-                <h1 className='2xl:text-3xl md:text-2xl p-2 sm:text-lg'>Zach Power</h1>
-                <img className='h-8' src='https://cdn-icons-png.flaticon.com/512/7838/7838457.png'></img>
+            <div id='logo' className='flex items-center gap-2 sm:gap-3'>
+                <h1 className='2xl:text-3xl md:text-2xl sm:p-2 sm:text-lg'>Zach Power</h1>
+                <img className='h-6 sm:h-8' src='https://cdn-icons-png.flaticon.com/512/7838/7838457.png'></img>
             </div>
             <nav>
-                <ul className='flex gap-5 sm:text-lg md:text-xl lg:text-xl'>
+                <ul className='flex gap-3 sm:gap-5 text-sm sm:text-lg md:text-xl lg:text-xl'>
                     <li className='py-3 sm:px-5  border-b-2 border-transparent hover:border-b-2 hover:border-purple'>
                         <Link to='/'>Home</Link>
                     </li>
