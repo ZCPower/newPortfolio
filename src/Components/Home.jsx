037e0c96f3b3e919645bd47102ed35345d5b8a8b
ZCPower <zachpowerxc@gmail.com>
2022-12-11 13:41:59 -0600

    page now checks view port, and renders a video on desktop viewports and a picture on mobile viewports

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -17,6 +17,24 @@ function Home() {
     const [greeting, setGreeting] = useState(textArr[0]);
     const [index, setIndex] = useState(0);
 
+
+    const [isMobile, setIsMobile] = useState(false)
+
+    //choose the screen size 
+    const handleResize = () => {
+        if (window.innerWidth < 720) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }
+
+    // create an event listener
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+    })
+
+
     // useEffect(() => {
     //     const timerId = setInterval(
     //         () => setIndex((i) => (i + 1) % textArr.length), // <-- increment index
@@ -35,8 +53,11 @@ function Home() {
         <motion.div id='homeContainer' className='flex flex-col justify-center
         '
         >
+            {!isMobile ?
+                <video autoPlay='autoplay' muted loop disablePictureInPicture controls={false} src={videoUrl} type='video/mp4' webkit-playsinline playsinline />
+                : <img id='videoPlaceholder' src='https://iili.io/HnpjS5l.jpg'></img>}
 
-            <video autoPlay='autoplay' muted loop disablePictureInPicture controls={false} src={videoUrl} type='video/mp4' webkit-playsinline playsinline />
+
 
             <div id='intro' className='gap px-5'>
                 <h2 className='text-3xl mb-5 my-4 py-2 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl type' ><Typewriter
